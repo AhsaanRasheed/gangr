236f9f79ab764ab24c7ef67b2daaaf498dbf41b5
Kamran <kamran@uforiainfotech.com>
2023-10-12 19:07:53 +0500

    Code improvement

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,6 +1,6 @@
 import { prefix1, prefix2 } from "./url"
 
-const getOrdersApi = prefix1 + 'getOrders?page=&' + prefix2
+const getOrdersApi = (num) => prefix1 + `getOrders?page=${num}&`+ prefix2
 const getOrderDetailApi = (id) => prefix1 + `orderDetail/${id}?`+ prefix2
 const xyz = prefix1 + 'orderDetail/';
 
